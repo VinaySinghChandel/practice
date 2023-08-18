bee71ed8fa28c4abd5e993e2dc388c3440c6ba0a
VinaySinghChandel <48399865+VinaySinghChandel@users.noreply.github.com>
2023-08-19 02:02:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link href="css/5.3.0/bootstrap.min.css" rel="stylesheet" />
+		<link href="bootstrap.min.css" rel="stylesheet" />
 		<title>Om Physiotherapy Center</title>
 	</head>
 	<body>
-		<div class="container p-2 ml-5 mt-2 border border-1 border-primary border-opacity-10 rounded">
+		<div class="container p-2">
 			<img src="omphysiotherapycenter.jpeg" class="img-fluid">
 		</div>		
 	</body>
