dbbbbe065297c126c036cc3c7ad556e0008daa38
VinaySinghChandel <48399865+VinaySinghChandel@users.noreply.github.com>
2023-08-19 02:02:04 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="css/5.3.0/bootstrap.min.css" rel="stylesheet" />
+		<title>Om Physiotherapy Center</title>
+	</head>
+	<body>
+		<div class="container p-2 ml-5 mt-2 border border-1 border-primary border-opacity-10 rounded">
+			<img src="omphysiotherapycenter.jpeg" class="img-fluid">
+		</div>		
+	</body>
+</html>
